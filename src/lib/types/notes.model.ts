07d4fc57671f1a65e350c8116a80d8f876b0292d
salmanrf <salmanrf2@gmail.com>
2023-03-31 14:54:33 +0700

    feat: notes & users api and models

---
+++ src/lib/types/notes.model.ts
@@ -0,0 +1,23 @@
+import { PaginationDto } from "src/lib/types/pagination.model";
+
+export interface Note {
+  note_id: string;
+  title: string;
+  content: string;
+  categories: string[];
+  created_at: Date | string;
+  updated_at: Date | string;
+}
+
+export interface FindNotesDto extends PaginationDto {
+  title?: string;
+  content?: string;
+  categories?: number[];
+}
+
+export interface CreateNoteDto {
+  title: string;
+  content: string;
+  categories: string[];
+  reference_url: string;
+}
