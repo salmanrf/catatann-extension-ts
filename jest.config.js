65f693b854201616dcee03f893741a903b7fda5c
salmanrf <salmanrf2@gmail.com>
2023-04-07 16:55:03 +0700

    test: test setup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^src/(.*)&": "<rootDir>/src/$1",
+    "^mocks/(.*)$": "<rootDir>/__mocks__/$1",
+  },
+  rootDir: ".",
+  moduleDirectories: ["node_modules", "src"],
+  testMatch: ["<rootDir>/__tests__/**/*.spec.ts"],
 };
